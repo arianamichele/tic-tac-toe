49a8a620048337a697470cf6a2ba7320605f77bb
YOUR-USERNAME <arianamichelebooking@gmail.com>
2022-06-16 07:04:40 -0700

    "tictactoe"

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let turn = playerX; {
 const boardState = Array(tiles.length);
 boardState.fill(null);
 
+
+ const strike = document.getElementById("strike");
 const completedGame = document.getElementById("completed-game");
 const gameOverText = document.getElementById("game-over-text");
 const playAgain = document.getElementById("play-again");
@@ -63,7 +65,7 @@ function tileClick(event) {
 
     function checkWinner() {
         for (const winningCombination of winningCombinations) {
-            const { combo, gameWin } = winningCombination;
+            const { combo, strikeClass } = winningCombination;
             const tileValue1 = boardState[combo[0] - 1];
             const tileValue2 = boardState[combo[1] - 1];
             const tileValue3 = boardState[combo[2] - 1];
@@ -73,7 +75,7 @@ function tileClick(event) {
                 tileValue1 === tileValue2 &&
                 tileValue1 === tileValue3
             ) {
-                gameWin.classList.add(gameWin);
+                strike.classList.add(strikeClass);
                 gameOverScreen(tileValue1);
                 return;
             }
@@ -96,7 +98,7 @@ function tileClick(event) {
     }
 
     function startNewGame() {
-        gameWin.className = "gameWin";
+        strike.className = "strike";
         completedGame.className = "hidden";
         boardState.fill(null);
         tiles.forEach((tile) => (tile.innerText = ""));
@@ -105,12 +107,12 @@ function tileClick(event) {
     }
 
     const winningCombinations = [
-        { combo: [1, 2, 3], gameWinClass: "gameWin-row-1" },
-        { combo: [4, 5, 6], gameWinClass: "gameWin-row-2" },
-        { combo: [7, 8, 9], gameWinClass: "gameWin-row-3" },
-        { combo: [1, 4, 7], gameWinClass: "gameWin-column-1" },
-        { combo: [2, 5, 8], gameWinClass: "gameWin-column-2" },
-        { combo: [3, 6, 9], gameWinClass: "gameWin-column-3" },
-        { combo: [1, 5, 9], gameWinClass: "gameWin-diagonal-1" },
-        { combo: [3, 5, 7], gameWinClass: "gameWin-diagonal-2" },
+        { combo: [1, 2, 3], strikeClass: "strike-row-1" },
+        { combo: [4, 5, 6], strikeClass: "strike-row-2" },
+        { combo: [7, 8, 9], strikeClass: "strike-row-3" },
+        { combo: [1, 4, 7], strikeClass: "strike-column-1" },
+        { combo: [2, 5, 8], strikeClass: "strike-column-2" },
+        { combo: [3, 6, 9], strikeClass: "strike-column-3" },
+        { combo: [1, 5, 9], strikeClass: "strike-diagonal-1" },
+        { combo: [3, 5, 7], strikeClass: "strike-diagonal-2" },
     ];
